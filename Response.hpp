54986d0bdea67b81a8f3809a7c87e9df2236a0d0
rferrero <rferrero@student.42sp.org.br>
2024-07-12 17:40:59 -0300

    finished dir on html

--- Response.hpp
+++ Response.hpp
@@ -12,11 +12,15 @@ private:
 
 				Response(void);
 
+	void		_file_validation(void);
+	void		_directory_validation(void);
 	void		_check_directory_location(void);
-	void		_check_file_location(void);
-	void		_check_file_empty(void);
 	void		_check_allowed_methods(void);
+	void		_check_directory_autoindex(void);
 	void		_check_errors_location_file(void);
+	void		_check_file_location(void);
+	void		_set_dir_content(void);
+	std::string	_get_dir_files(void);
 
 	void		_make_response(void);
 	void		_send_response(void);
